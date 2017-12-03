d9810c47de0a120e7b91e346333f2be120226fd3
Colin <colin.r.fraser@gmail.com>
2017-12-02 17:23:02 -0800

    eval notebook

--- address_compare/evaluate.py
+++ address_compare/evaluate.py
@@ -2,19 +2,21 @@ import pandas as pd
 from sklearn.metrics import confusion_matrix
 from address_compare.comparers import naive_compare
 
-DEFAULT_TEST_DATA = 'data/compare_train.csv'
+DEFAULT_TEST_DATA = './data/compare_train.csv'
 
-def evaluate_comparer(comparer, testing_csv = DEFAULT_TEST_DATA):
+def evaluate_comparer(comparer, testing_data = DEFAULT_TEST_DATA):
     '''
 
     :param comparer: a comparer function that takes two strings and returns true if they represent the same address
-    :param df: the path to a csv where the first two columns
+    :param df: either a dataframe, or the path to a csv where the first two columns
     :return:
     '''
-    
-    df = pd.read_csv(DEFAULT_TEST_DATA)
 
-    pairs = df.iloc[:, [0,1]]
+    if type(testing_data) == str:
+        testing_data = pd.read_csv(DEFAULT_TEST_DATA)
+
+
+    pairs = testing_data.iloc[:, [0,1]]
     result = [comparer(a, b) for i, a, b in pairs.itertuples()]
-    return pd.DataFrame(dict(actual = df.iloc[:, 2], predicted = result))
+    return pd.DataFrame(dict(actual = testing_data.iloc[:, 2], predicted = result))
 
