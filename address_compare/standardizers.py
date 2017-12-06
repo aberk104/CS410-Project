11c07ba5b9465caade1a6bde4ec7f2631669dbbe
aberk104 <aberk104@yahoo.com>
2017-12-05 21:04:09 -0500

    removal of the non-functionalized code (the calls to the reference data file) as they will now be in the master_control_file.py

--- address_compare/standardizers.py
+++ address_compare/standardizers.py
@@ -3,38 +3,6 @@ The standardizers.py file contains functions to help standardize tagged strings 
 '''
 
 from collections import OrderedDict
-from collections import defaultdict
-from address_compare import reference_data as refdt
-
-'''
-the following function calls and nested_ref_dt_dict creation will likely be better served in the file that eventually calls the standardizer function itself
-'''
-street_types = refdt.all_us_street_types()
-unit_types = refdt.all_us_unit_types()
-states = refdt.all_us_states()
-compass_points_def, key_val_switch_compass_pts = refdt.compass_points()
-
-all_caps_street_types_dict = dict()
-for row in range(street_types.shape[0]):
-    all_caps_street_types_dict[street_types.loc[row, 'st_abbrev'].upper()] = street_types.loc[row, 'street_type'].upper()
-
-all_caps_unit_types_dict = dict()
-for row in range(unit_types.shape[0]):
-    all_caps_unit_types_dict[unit_types.loc[row, 'unit_type_abbrev'].upper()] = unit_types.loc[row, 'unit_type_name'].upper()
-
-all_caps_states_dict = dict()
-for row in range(states.shape[0]):
-    all_caps_states_dict[states.loc[row, 'Postal Code'].upper()] = states.loc[row, 'State'].upper()
-    if states.notnull().loc[row, 'Abbreviation']:
-        all_caps_states_dict[states.loc[row, 'Abbreviation'].upper()] = states.loc[row, 'State'].upper()
-
-# the following creates a nested dictionary where the first keys are the tags in the ordered dictionary
-nested_ref_dt_dict = defaultdict(dict)
-nested_ref_dt_dict['UNIT_TYPE'] = all_caps_unit_types_dict
-nested_ref_dt_dict['STREET_TYPE'] = all_caps_street_types_dict
-nested_ref_dt_dict['PRE_DIRECTION'] = key_val_switch_compass_pts
-nested_ref_dt_dict['POST_DIRECTION'] = key_val_switch_compass_pts
-nested_ref_dt_dict['STATE'] = all_caps_states_dict
 
 
 
