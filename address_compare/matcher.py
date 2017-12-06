36a717ba913eccd32a6529b080f367704a0fd68f
aberk104 <aberk104@yahoo.com>
2017-12-05 20:35:35 -0500

    added documentation to the match function

--- address_compare/matcher.py
+++ address_compare/matcher.py
@@ -3,7 +3,18 @@ The matcher.py file contains a match function to compare 2 lists of addresses.
 It calls the separate exact and learning compare comparers in order to find a matching address in list 2 for each address in list 1
 '''
 
-def match(address_list_1, address_list_2, compare_function):
+def match(address_list_1: list, address_list_2: list, compare_function):
+    '''
+    The match function takes 2 lists of addresses and passes them to a defined compare function.
+    The compare function returns a true/false indicating whether or not an address in list 1 is a match to an address in list 2.
+    The record IDs for the matching addresses are added to the matched_pairs list as a tuple.
+    :param address_list_1: this is the first list of addresses
+    :param address_list_2: this is the second list of addresses
+    :param compare_function: the function that will be used to find matching addresses between the 2 lists
+    :return: 
+    '''
+
+
     matched_pairs = list()
     address_list_1_copy = address_list_1.copy()
 
@@ -27,5 +38,3 @@ def match(address_list_1, address_list_2, compare_function):
     unmatched_address_list_2 = list()
     for item in address_list_2:
         unmatched_address_list_2.append((item, "NA")) #this will eventually get changed to Record IDs instead of the full orderedDict
-
-    
