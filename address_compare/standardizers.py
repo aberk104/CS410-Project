faef92dbe70fd0320e4bd37bf6421e4c9a9a1225
aberk104 <aberk104@yahoo.com>
2017-12-08 12:00:28 -0500

    added a groupby function to group a tagged address list

--- address_compare/standardizers.py
+++ address_compare/standardizers.py
@@ -89,6 +89,16 @@ def standardizer(ordered_dict, nested_reference_dictionary = nested_ref_dt_dict)
 
 
 
+def consolidate_address_list(address_df):
+    column_names_for_group = ['UNIT_TYPE','UNIT_NUMBER','STREET_NUMBER','PRE_DIRECTION','STREET_NAME','STREET_TYPE','POST_DIRECTION','UNKNOWN','CITY','STATE','ZIP_CODE']
+    grouped_df = address_df.groupby(column_names_for_group)['Record_ID'].apply(list)
+    return grouped_df
+
+
+
+
+
+
 # testdict = OrderedDict([('UNIT_TYPE', ['Bldg', 'Apt']),
 # ('UNIT_NUMBER', ['1', '1']),
 # ('STREET_NUMBER', ['1']),
