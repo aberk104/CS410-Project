09bf359981a7f6cc6286813d0a828f9d81067a65
aberk104 <aberk104@yahoo.com>
2017-12-09 15:17:27 -0500

    creation of an address randomizer to create raw test data

---
+++ address_compare/address_randomizer.py
@@ -0,0 +1,56 @@
+'''
+The address_randomizer will create a specified number of random raw addresses that can be used for testing purposes
+'''
+
+import pandas as pd
+import random
+
+available_unit_types = ['APT','BLDG','STE','FL','PH','UNIT',""]
+available_street_types = ['RD','DR','AVE','LN','BLVD','PKWY',""]
+available_street_names = ['MAPLE','ELM','MAIN','WALL','BROAD','GEORGE ALLEN','FULTON']
+available_directionals = ['E','S','N','W',""]
+available_unit_nums = ['1','2','3','4','5','6',""]
+
+def random_addresses(num_addresses: int):
+    '''
+    This creates a random list of raw addreses that can be used for testing purposes
+    :param num_addresses: this is an integer containing the number of addresses the user wants to create
+    :return: new_address_df - a single column dataframe where the column name is "Single String Address" and the values are the randomized raw addresses
+    '''
+    new_addresses = list()
+
+    for num in range(num_addresses):
+        address = None
+        unit_type = None
+        unit_num = None
+        pre_directional = None
+        post_directional = None
+
+        if random.choice([True,False]):
+            unit_type = random.choice(available_unit_types)
+            unit_num = random.choice(available_unit_nums)
+
+        if random.choice([True, False]):
+            pre_directional = random.choice(available_directionals)
+
+        if random.choice([True, False]):
+            post_directional = random.choice(available_directionals)
+
+        street_num = str(random.randint(1, 21))
+        street_name = random.choice(available_street_names)
+        street_type = random.choice(available_street_types)
+
+        portions = [unit_type, unit_num, street_num, pre_directional, street_name, post_directional, street_type]
+
+        for item in portions:
+            if item != None:
+                if address == None and len(item) > 0:
+                    address = item
+                elif len(item) > 0:
+                    address += str(" ") + item
+
+        new_addresses.append(address)
+
+    new_address_df = pd.DataFrame(data=new_addresses, columns=['Single String Address'])
+
+    return new_address_df
