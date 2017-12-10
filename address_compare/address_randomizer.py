eab18c6b6fe0d6d7bc3d5a622fb19b0723857a2b
aberk104 <aberk104@yahoo.com>
2017-12-09 20:26:57 -0500

    changed weights of items in random.choice lists

--- address_compare/address_randomizer.py
+++ address_compare/address_randomizer.py
@@ -8,10 +8,10 @@ import random
 available_unit_types = ['APT','BLDG','STE','FL','PH','UNIT',""]
 available_street_types = ['RD','DR','AVE','LN','BLVD','PKWY',""]
 available_street_names = ['MAPLE','ELM','MAIN','WALL','BROAD','GEORGE ALLEN','FULTON']
-available_directionals = ['E','S','N','W',""]
+available_directionals = ['E','S','N','W',""*5]
 available_unit_nums = ['1','2','3','4','5','6']
 
-def random_addresses(num_addresses: int):
+def random_addresses(num_addresses: int, raw_address_col_name = 'Single String Address'):
     '''
     This creates a random list of raw addreses that can be used for testing purposes
     :param num_addresses: this is an integer containing the number of addresses the user wants to create
@@ -26,14 +26,14 @@ def random_addresses(num_addresses: int):
         pre_directional = None
         post_directional = None
 
-        if random.choice([True,False]):
+        if random.choice([True,False*4]):
             unit_type = random.choice(available_unit_types)
-            unit_num = random.choice(["#","","","","","",""]) + random.choice(available_unit_nums)
+            unit_num = random.choice(["#",""*10]) + random.choice(available_unit_nums)
 
-        if random.choice([True, False]):
+        if random.choice([True, False*4]):
             pre_directional = random.choice(available_directionals)
 
-        if random.choice([True, False]):
+        if random.choice([True, False*4]):
             post_directional = random.choice(available_directionals)
 
         street_num = str(random.randint(1, 21))
@@ -41,8 +41,8 @@ def random_addresses(num_addresses: int):
         street_type = random.choice(available_street_types)
 
         address_formats = {
-            1: [unit_type, unit_num, random.choice(["-","","","","","",""]) if unit_num != None else "", street_num, pre_directional, street_name, post_directional, street_type],
-            2: [street_num, pre_directional, street_name, post_directional, street_type, random.choice([",","","","","","",""]) if unit_num != None else "", unit_type, unit_num]}
+            1: [unit_type, unit_num, random.choice(["-",""*10]) if unit_num != None else "", street_num, pre_directional, street_name, post_directional, street_type],
+            2: [street_num, pre_directional, street_name, post_directional, street_type, random.choice([",",""*10]) if unit_num != None else "", unit_type, unit_num]}
 
         selected_format = random.randint(1,2)
 
@@ -55,6 +55,6 @@ def random_addresses(num_addresses: int):
         address = str.replace(address," ,",",") if random.choice([True,False]) else address
         new_addresses.append(address)
 
-    new_address_df = pd.DataFrame(data=new_addresses, columns=['Single String Address'])
+    new_address_df = pd.DataFrame(data=new_addresses, columns=[raw_address_col_name])
 
     return new_address_df
