83745ef2504152225fd4d6c225c4bb89c2d08438
aberk104 <aberk104@yahoo.com>
2017-12-20 22:37:05 -0500

    slight test update to conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('C:\Users\Alan\Documents\GitHub\CS410-Project'))
+#sys.path.insert(0, os.path.abspath('C:\Users\Alan\Documents\GitHub\CS410-Project'))
 
 
 # -- General configuration ------------------------------------------------
